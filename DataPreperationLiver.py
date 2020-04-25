dfe5985977225a141565d0f0e6694d2f206476b0
sibsmc <siobhanmcloughlin3@gmail.com>
2020-04-25 19:34:30 +0200

    prepare data files liver and pancreas

--- DataPreperationLiver.py
+++ DataPreperationLiver.py
@@ -15,13 +15,13 @@ NNdata = NNdata.rename(columns={'Hardy.Scale':'Hardy_Scale'})
 NNdata = NNdata.rename(columns={'Pathology.Categories_pancreas':'Pathology_Categories_pancreas', 'Pathology.Categories_liver': 'Pathology_Categories_liver'})
 
 NNdataPan=NNdata.copy()
-NNdataPan=NNdataPan.drop(columns=['Fat_Percentage_liver', 'Pathology_Categories_liver'])
+NNdataPan=NNdataPan.drop(columns=['Fat_Percentage_pancreas', 'Pathology_Categories_pancreas'])
 short_data2=NNdataPan.copy()
 
 from sklearn.preprocessing import MultiLabelBinarizer
 mlb = MultiLabelBinarizer()
-mask = short_data2['Pathology_Categories_pancreas'].notnull()
-arr = mlb.fit_transform(short_data2.loc[mask, 'Pathology_Categories_pancreas'].dropna().str.split(', '))
+mask = short_data2['Pathology_Categories_liver'].notnull()
+arr = mlb.fit_transform(short_data2.loc[mask, 'Pathology_Categories_liver'].dropna().str.split(', '))
 df = (pd.DataFrame(arr, index=short_data2.index[mask], columns=mlb.classes_).reindex(short_data2.index, fill_value=0))
 
 # # check all values are 0 or 1
@@ -32,6 +32,8 @@ Final_Data = NNdataPan.join(df)
 Final_Data = Final_Data.join(pd.get_dummies(Final_Data['Sex'],prefix='Sex'))
 Final_Data = Final_Data.join(pd.get_dummies(Final_Data['Hardy_Scale'],prefix='Hardy'))
 Final_Data=Final_Data.join(pd.get_dummies(Final_Data['Age_Bracket'],prefix='Age'))
-Final_Data=Final_Data.drop(columns=['Age_Bracket','Hardy_Scale','Sex', 'Pathology_Categories_pancreas'])
+Final_Data=Final_Data.drop(columns=['Age_Bracket','Hardy_Scale','Sex', 'Pathology_Categories_liver'])
 
-# Final_Data.to_csv(r'insert path here', index=False)
+# Final_Data.to_csv(r'insert_path', index=False)
+
+
